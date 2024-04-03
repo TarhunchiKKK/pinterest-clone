87b9c2fefb56d8fb5dbbcc5fa2fdf5df8873325b
Tarhun <TarhunchicKKK@yandex.by>
2024-04-03 17:43:59 +0300

    Add search for posts

--- src/js/posts/index.js
+++ src/js/posts/index.js
@@ -4,18 +4,29 @@ import { getColumnsCount } from "./utils/getColumnsCount";
 
 let posts = JSON.parse(localStorage.getItem('posts')) ?? [];
 
-function renderPosts() {
+function renderPosts(renderList = posts) {
     const postsContainer = document.getElementById('posts-container')
     
     const postsWrappersCount = getColumnsCount();
 
-    const splitedPosts = splitPosts(posts, postsWrappersCount);
+    const splitedPosts = splitPosts(renderList, postsWrappersCount);
     const postsWrappers = splitedPosts.map(item => createPostsWrapper(item));
     for (let postsWrapper of postsWrappers) {
         postsContainer.appendChild(postsWrapper)
     }
 }
 
+function searchPosts(title) {
+    return posts.filter(post => post.title.includes(title.toLowerCase()));
+}
+
+function savePosts() {
+    localStorage.setItem('posts', JSON.stringify(posts));
+    posts = JSON.parse(localStorage.getItem('posts'));
+}
+
 export {
-    renderPosts
+    renderPosts,
+    searchPosts,
+    savePosts
 }
